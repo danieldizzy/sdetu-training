e96e3d35ac0c0a213b5973c486fef83c589fc24c
Daniel <dizzylol4@gmail.com>
2018-01-12 21:42:44 +0900

    try error

---
+++ src/exceptionsandfiles/CommonExceptions.java
@@ -0,0 +1,30 @@
+package exceptionsandfiles;
+
+public class CommonExceptions {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        //1. Identify the potential problem area
+        //2. surround with a try catch block
+
+        try {
+            int c = a/b;
+        }catch (ArithmeticException e){
+            System.out.println("CANNOT DIVIDE BY ZERO");
+            System.out.println(e.toString());
+        }
+
+        String[] states = {"CA", "TX", "FL", "NY"};
+        for (int i = 0; i <= states.length; i++) {
+            try {
+                System.out.println(states[i]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("ERROR: Index outside of array domain");
+                System.out.println(e.toString());
+            }finally {
+                System.out.println("Iterating through array");
+            }
+
+        }
+    }
+}
