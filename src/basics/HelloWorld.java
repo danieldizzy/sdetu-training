30c18735b8c5fad7a425ee84eddb668b05795c85
Daniel Agbolozo <dizzylol4@gmail.com>
2018-01-12 15:39:26 +0900

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is a change in code");
+        System.out.println("Adding code from git hub");
     }
 
 }
