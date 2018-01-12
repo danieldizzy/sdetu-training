522ec91d5210f44fd5af257b139381f7b7c7eb9b
Daniel <dizzylol4@gmail.com>
2018-01-12 15:34:20 +0900

    basic change

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,5 +3,7 @@ package basics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is a change in code");
     }
+
 }
